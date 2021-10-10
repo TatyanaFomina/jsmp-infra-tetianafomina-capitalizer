68640a99733ca7cfbb2ef790b5e617c7dc5ff7ff
Tetiana Fomina1 <Tetiana_Fomina1@epam.com>
2021-10-10 21:57:13 +0300

    filled npmignore

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
+import winston, { Logger, LoggerOptions } from 'winston';
+
 function capitalize(str: string): string {
-    let regexp = /\b[a-z]/g;
+    const loggerOptions: LoggerOptions = {      
+        format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+        transports: [new winston.transports.Console()]
+    };
+    const logger: Logger = winston.createLogger(loggerOptions);
+    const regexp = /\b[a-z]/g;
+    
+    logger.info('Your string capitalized');
     return str.replace(regexp, (str) => str.toUpperCase());
 }
 
